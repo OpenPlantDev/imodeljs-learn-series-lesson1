175702537337eca7960a28c0c3305c34201f0cd5
Daniel Nichols <danielrnichols63@gmail.com>
2019-04-15 18:17:41 -0500

    Initial cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <span>My React App</span>
         </header>
+        <div>
+          <h4>My React App Content</h4>
+        </div>
       </div>
     );
   }
